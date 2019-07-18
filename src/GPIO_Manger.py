d3ac1c3d694113b892cab4fa601767d559d668de
Carter Frost <carter90@gmail.com>
2019-07-18 09:10:11 -0700

    GPIO_Manger.py: fix syntax error, gpio pin, servo setup, fix sleep call, fix self references

--- src/GPIO_Manger.py
+++ src/GPIO_Manger.py
@@ -7,14 +7,16 @@ class GPIO_Manger:
 	_modelights = [25, 12, 16, 20]
 	_modebuttons = [ 5, 13, 19, 26, 23 ]
 	_buttonNames = ["Pause/UnPause button", "Mode button 1", "Mode button 2", "Mode button 3", "Mode button 4"]
+	_servopin = 6
 	def __init__(self): 
 		GPIO.setmode(GPIO.BCM)
-		self.servo = GPIO.PWM(12, 50)
-		self.servo.start(_bins[0])
-		GPIO.setup(_statelights, GPIO.OUT)
-		GPIO.setup(_modelights, GPIO.OUT)
-		GPIO.setup(_modebuttons, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-		for pin in _modebuttons:  # set button pins to 3.3v
+		GPIO.setup(self._servopin, GPIO.OUT)
+		self.servo = GPIO.PWM(self._servopin, 50)
+		self.servo.start(self._bins[0])
+		GPIO.setup(self._statelights, GPIO.OUT)
+		GPIO.setup(self._modelights, GPIO.OUT)
+		GPIO.setup(self._modebuttons, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+		for pin in self._modebuttons:  # set button pins to 3.3v
 			#GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #PUD_DOWN or PUD_UP
 			GPIO.add_event_detect(pin, GPIO.RISING, callback=self.button_pressed, bouncetime=1500) #was GPIO.FALLING
 	
@@ -24,7 +26,7 @@ class GPIO_Manger:
 	
 	#moves the servo to the bin
 	def sweepto(self, bin_num=0): 
-		self.servo.ChangeDutyCycle(_bins[bin_num]) 
+		self.servo.ChangeDutyCycle(self._bins[bin_num]) 
 	
 	#TODO: Feed till optical switch is tripped then return
 	def feed(self):
@@ -37,17 +39,17 @@ class GPIO_Manger:
 	def button_pressed(self, channel):
 		print("Button Pressed", self._buttonNames[self._modebuttons.index(channel)])
 	
-	def test_loop(self);
+	def test_loop(self):
 		while True: 
 			for x in range(3):
 				self.sweepto(x)
-				time.sleep(.5)
-			for pin in _statelights + _modelights:
+				sleep(.5)
+			for pin in self._statelights + self._modelights:
 				GPIO.output(pin,GPIO.HIGH) # on
-				time.sleep(.2)
-			for pin in _statelights + _modelights:
+				sleep(.2)
+			for pin in self._statelights + self._modelights:
 				GPIO.output(pin,GPIO.LOW) #off
-				time.sleep(.2)
+				sleep(.2)
 				
 if __name__ == "__main__":
 	gm = GPIO_Manger()
